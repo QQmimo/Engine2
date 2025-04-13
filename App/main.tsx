4d9e75bafc5982f1ae5a8c9dcb0ed04e64daf2f8
QQmimo <qqmimo@yandex.ru>
2025-04-13 19:24:00 +0500

    Правки

--- App/main.tsx
+++ App/main.tsx
@@ -15,16 +15,16 @@ for (let i: number = 0; i < 350; i++) {
     obj.addComponent(Moved);
     obj.addComponent(Physic);
 
-    const obj_moved = obj.getComponent(Moved);
-    obj_moved.Speed = 25;
-    obj_moved.Target = Random.Vector2D(screen.Width, screen.Height);
-    obj_moved.onFinish((_o, c) => {
+    const moved = obj.getComponent(Moved);
+    moved.Speed = 25;
+    moved.Target = Random.Vector2D(screen.Width, screen.Height);
+    moved.onFinish((_o, c) => {
         c.Target = Random.Vector2D(screen.Width, screen.Height);
     });
 
-    const obj_physic: Physic = obj.getComponent(Physic);
-    obj_physic.Mass = Random.Integer(obj.Transform.Size.Width, 25);
-    obj_physic.onCollision((o) => {
+    const physic: Physic = obj.getComponent(Physic);
+    physic.Mass = Random.Integer(obj.Transform.Size.Width, 25);
+    physic.onCollision((o) => {
         o.Transform.Fill = "#f00";
         setTimeout(() => {
             o.Transform.Fill = "grey";
