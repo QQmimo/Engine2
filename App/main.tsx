391bd5e8a221b6d0e2d1bfb7f6d696dc6b12ee4c
QQmimo <qqmimo@yandex.ru>
2025-04-17 00:26:01 +0500

    Удаляются точки гравитации

--- App/main.tsx
+++ App/main.tsx
@@ -7,25 +7,33 @@ const scene: GameScene = screen.addScene();
 const layer: GameLayer = scene.addLayer();
 
 screen.Canvas.onclick = (e) => {
-    //const oldBlack: GameObject[] = GameObject.selectByTag('black hole');
-    //oldBlack.forEach(old => old.destroy());
     const black: GameObject = new GameObject(layer);
     black.addTag('black hole');
     black.Transform.Position = new Vector2D(e.clientX, e.clientY);
     black.Transform.Fill = 'black';
     black.Transform.Size = new Size(5, 0);
     black.addComponent(Drawed);
+    black.addComponent(Physic);
+    const physic: Physic = black.getComponent(Physic);
+    physic.Mass = Infinity;
+    physic.Flex = 0;
+    black.addComponent(Moved);
     black.onUpdate(() => {
         const all: GameObject[] = GameObject.selectByComponent(Physic);
         all.forEach(o => {
             const distance: number = black.Transform.Position.distance(o.Transform.Position);
             o.getComponent(Moved).Velocity = new Vector2D(
-                (black.Transform.Position.X - o.Transform.Position.X) / (distance),
-                (black.Transform.Position.Y - o.Transform.Position.Y) / (distance)
+                (black.Transform.Position.X - o.Transform.Position.X) / (distance * distance + 0.01),
+                (black.Transform.Position.Y - o.Transform.Position.Y) / (distance * distance + 0.01)
             );
         });
     });
 }
+screen.Canvas.oncontextmenu = (e => {
+    e.preventDefault();
+    const oldBlack: GameObject[] = GameObject.selectByTag('black hole');
+    oldBlack.forEach(old => old.destroy());
+});
 
 for (let i: number = 0; i < 500; i++) {
     const obj: GameObject = new GameObject(layer);
@@ -45,6 +53,12 @@ for (let i: number = 0; i < 500; i++) {
 
     const physic: Physic = obj.getComponent(Physic);
     physic.Mass = obj.Transform.Size.Width * obj.Transform.Size.Width;
+    physic.onCollision(o => {
+        if (!o.Tags.includes('black hole')) {
+            o.Transform.Fill = 'red';
+            setTimeout(() => { o.Transform.Fill = 'green'; }, 200);
+        }
+    });
 }
 
 screen.play();
